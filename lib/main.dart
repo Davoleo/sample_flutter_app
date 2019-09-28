9cb57170f4980472d1c8e3626fc41f7f6ec40e97
Davoleo <davoleo2001@gmail.com>
2019-09-28 19:20:57 +0200

    Flexible - MaterialApp - TextField

--- lib/main.dart
+++ lib/main.dart
@@ -15,24 +15,71 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey,
-      padding: const EdgeInsets.all(26.0),
-      margin: EdgeInsets.only(top:30),
-      alignment: Alignment.topCenter,
-      child: Column(
-        //Flexbox Styled widget positioning
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        textDirection: TextDirection.ltr,
-        children: <Widget>[
-          Container(color: Colors.red, child: Text(this.text, textDirection: TextDirection.ltr,),) ,
-          Container(color: Colors.blue, child: Text("Brief Text", textDirection: TextDirection.ltr,),),
-          Container(color: Colors.green, child: Text("Very very crazy long text", textDirection: TextDirection.ltr,),),
-        ],
-      ),
+    return MaterialApp(
+      title: "SampleFlutterApp",
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: LoginPage(),
     );
   }
 
 }
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // The structure of the content of the app
+    return Scaffold(
+      appBar: AppBar(title: Text("Positioning"),),   //Colored App bar at the top
+      body: Container(                               //Body: Contains all the content
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //Title Box
+            Container(margin: EdgeInsets.only(bottom: 16), child: Text("LOGIN"),),
+            //Username Box
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              child: Row(
+                children: <Widget>[
+                  Container(child: Text("USERNAME"), margin: EdgeInsets.only(right: 8),),
+                  Flexible(child: TextField(),)
+                ],),
+            ),
+            // Password Box
+            Container(
+              margin: EdgeInsets.only(bottom: 16),
+              child: Row(
+                children: <Widget>[
+                  Container(child: Text("PASSWORD"), margin: EdgeInsets.only(right: 8),),
+                  Flexible(child: TextField(obscureText: true,),)
+                ],),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[RaisedButton(child: Text("Login"), onPressed: () => {},)],
+            )
+          ],),
+      ),
+    );
+  }
+}
+
+//return Container(
+//color: Colors.grey,
+//padding: const EdgeInsets.all(26.0),
+//margin: EdgeInsets.only(top:30),
+//alignment: Alignment.topCenter,
+//child: Column(
+////Flexbox Styled widget positioning
+//mainAxisAlignment: MainAxisAlignment.start,
+//mainAxisSize: MainAxisSize.max,
+//crossAxisAlignment: CrossAxisAlignment.stretch,
+//textDirection: TextDirection.ltr,
+//children: <Widget>[
+//Container(color: Colors.red, child: Text(this.text, textDirection: TextDirection.ltr,),) ,
+//Container(color: Colors.blue, child: Text("Brief Text", textDirection: TextDirection.ltr,),),
+//Container(color: Colors.green, child: Text("Very very crazy long text", textDirection: TextDirection.ltr,),),
+//],
+//),
+//);
