bdfdfdfd352c0c8aba2c7a225e594b77c53e5cf3
Davoleo <davoleo2001@gmail.com>
2020-04-04 18:46:40 +0200

    Convert the StarRating Widget to Stateless

--- lib/components/StarRating.dart
+++ lib/components/StarRating.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
 
-class StarRating extends StatefulWidget {
-  final void Function(int value) onPressed;
+class StarRating extends StatelessWidget {
   final int value;
+  final void Function(int value) onPressed;
 
 
-  StarRating({@required this.onPressed, this.value = 0, Key key}) : super(key: key);
+  StarRating({@required this.value, @required this.onPressed, Key key}) : super(key: key);
 
-  @override
-  _StarRatingState createState() => _StarRatingState();
-}
-
-class _StarRatingState extends State<StarRating> {
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: List.generate(5, (index) {
         return InkWell(
-          child: getStar(index),
-          onTap: () => widget.onPressed(index),
+          child: _getStar(index),
+          onTap: () => onPressed(index),
         );
-      }),
+      })
     );
   }
 
-  Icon getStar(int pos) {
-    if (pos <= widget.value)
+  Icon _getStar(int pos) {
+    if (pos <= value)
       return Icon(Icons.star, color: Colors.orange,);
     else
       return Icon(Icons.star_border, color: Colors.grey,);
